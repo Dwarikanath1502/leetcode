f47eef279c72c430e8129ac0c2f0386de733f22e
Dwarikanath <68079395+Dwarikanath1502@users.noreply.github.com>
2023-09-05 11:20:36 +0530

    Time: 71 ms (15.22%) | Memory: 72.6 MB (6.48%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,31 +1,34 @@
 class Solution {
 
-void findCombination(int index, int target, vector<int> &ds, vector<int> &candidates, vector<vector<int>> &ans)
-{
-    if(index == candidates.size()){
-        if(target == 0){
-            ans.push_back(ds);
-        }
-        return;
-    }
+	void findCombinations(int index, int target, vector<int> ds, vector<int> combinations, vector<vector<int>> &ans)
+		{
+			if(index == combinations.size())
+			{
+				if(target == 0)
+				{
+					ans.push_back(ds);
+				}
+		return ;
+		}
+		//include call
+		if(combinations[index] <= target)
+		{
+			ds.push_back(combinations[index]);
+			findCombinations(index, target - combinations[index], ds, combinations,     ans);
+		ds.pop_back();
+		}
 
-    // include
-    if(candidates[index] <= target){
-        ds.push_back(candidates[index]);
-        findCombination(index, target - candidates[index], ds, candidates, ans);
-        ds.pop_back();
-        
-    }
-    // exclude
-    findCombination(index + 1, target, ds, candidates, ans);
-}
+		//exclude call
+		findCombinations(index + 1, target, ds, combinations, ans);
+	}
+
 
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> ds;
         int index = 0;
-        findCombination(index, target, ds, candidates, ans);
+        findCombinations(index, target, ds, candidates, ans);
         return ans;
     }
 };
