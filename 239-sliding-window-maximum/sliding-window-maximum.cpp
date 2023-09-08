c2c8ad561be5f80b06c832936c522aba78871ef6
Dwarikanath <68079395+Dwarikanath1502@users.noreply.github.com>
2023-09-08 23:09:45 +0530

    Time: 209 ms (61.13%) | Memory: 134.7 MB (73.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,24 +4,23 @@ public:
       deque<int> dq;
       vector<int> ans;
       int size = nums.size();
-// storing the index of the element in the queue
-      for(int i = 0 ; i < size; i++){
-        //   remove the index if it is getting out of bound
-        if(!dq.empty() && dq.front() == i - k){
-            dq.pop_front();
-        }
-        // remove the element smaller than nums[i]
-        while(!dq.empty() && nums[dq.back()] <= nums[i]){
-            dq.pop_back();
-        }
-        //push to queue
-        dq.push_back(i);
-        //push the element which is at first index in the deque
-
-        if( i >= k- 1){
-            ans.push_back(nums[dq.front()]);
-        }
+  // storing the index of the element in the queue
+    for(int i = 0; i < size; i++){
+      // remove out of bound element
+      if(!dq.empty() && dq.front() == i - k){
+        dq.pop_front();
       }
+      // remove smaller than numsp[i]
+      while(!dq.empty() && nums[dq.back()] <= nums[i]){
+        dq.pop_back();
+      }
+      // else push i
+      dq.push_back(i);
+
+      if(i >= k - 1 ){
+        ans.push_back(nums[dq.front()]);
+      }
+    }
     return ans;
     }
 };
