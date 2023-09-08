4fa1693ef46241a8abc67ac322695796f3f18418
Dwarikanath <68079395+Dwarikanath1502@users.noreply.github.com>
2023-09-08 22:54:04 +0530

    Time: 199 ms (77.79%) | Memory: 134.7 MB (53.17%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       deque<int> dq;
-       vector<int> ans;
-
-        int size = nums.size();
-
-        for(int i = 0; i < size; i++){
-            // remove out of bound
-            if(!dq.empty() && dq.front()== i - k){
-                dq.pop_front();
-            }
-        //remove all smaller element
+      deque<int> dq;
+      vector<int> ans;
+      int size = nums.size();
+// storing the index of the element in the queue
+      for(int i = 0 ; i < size; i++){
+        //   remove the index if it is getting out of bound
+        if(!dq.empty() && dq.front() == i - k){
+            dq.pop_front();
+        }
+        // remove the element smaller than nums[i]
         while(!dq.empty() && nums[dq.back()] <= nums[i]){
             dq.pop_back();
         }
-        // push element
+        //push to queue
         dq.push_back(i);
-            // if erached at kth element push the max to the ans
-        if(i >= k-1){
+        //push the element which is at first index in the deque
+
+        if( i >= k- 1){
             ans.push_back(nums[dq.front()]);
         }
-        }
+      }
     return ans;
     }
 };
